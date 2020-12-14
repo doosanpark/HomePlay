089f066510a8a31b39ae6cb60a98571245017cc2
DESKTOP-NKSGQVR\C <chch4803@naver.com>
2020-12-14 18:01:46 +0900

    상세화면 (상)

---
+++ src/main/java/com/oraclejava/spring/model/detail/Detail.java
@@ -0,0 +1,47 @@
+package com.oraclejava.spring.model.detail;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "R_MOVIE")
+@Getter
+@Setter
+public class Detail {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+	generator = "movieNoGenerator")
+	@SequenceGenerator(name = "movieNoGenerator",
+		sequenceName = "MOVIE_NO_SEQ",
+			allocationSize = 1)
+	
+	private int no;
+	private String title;
+	private String genre;
+	private String summary_title;
+	private String summary_content;
+	private String age_limit;
+	private String poster;
+	private String thumbnail;
+	private String screening;
+	private String reg_date;
+	
+	/*
+	 * public Detail(int nO, String title, String genre, String summary_title,
+	 * String summary_content, int age_limit, String poster, String thumbnail,
+	 * String screening, int reg_date) { super(); NO = nO; this.title = title;
+	 * this.genre = genre; this.summary_title = summary_title; this.summary_content
+	 * = summary_content; this.age_limit = age_limit; this.poster = poster;
+	 * this.thumbnail = thumbnail; this.screening = screening; this.reg_date =
+	 * reg_date; }
+	 */
+	
+	
+}
