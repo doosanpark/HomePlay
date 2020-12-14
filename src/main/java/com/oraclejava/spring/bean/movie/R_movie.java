3fdfeebea7a91d514370f371b9dea51ffb0d46d2
YANG95 <adolf9608@gmail.com>
2020-12-14 15:15:19 +0900

    movie bean,controller,repository 추가 moviebean.html 추가했습니다.

---
+++ src/main/java/com/oraclejava/spring/bean/movie/R_movie.java
@@ -0,0 +1,35 @@
+package com.oraclejava.spring.bean.movie;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class R_movie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+	generator = "movieIdGenerator")
+	@SequenceGenerator(name = "movieIdGenerator",
+	sequenceName = "movie_no_seq",
+	allocationSize = 1)
+	private int no;
+    private String title;
+    private String genre;
+    private String summary_title;
+    private String summary_content;
+    private String age_limit;
+    private String poster;
+    private String thumbnail;
+    private String screening;
+    private String reg_date;
+    
+    
+}
--- src/main/java/com/oraclejava/spring/dao/movie/MovieRepository.java
+++ src/main/java/com/oraclejava/spring/dao/movie/MovieRepository.java
@@ -2,8 +2,8 @@ package com.oraclejava.spring.dao.movie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.oraclejava.spring.model.movie.MovieBean;
+import com.oraclejava.spring.bean.movie.R_movie;
 
-public interface MovieRepository extends JpaRepository<MovieBean, Integer> {
+public interface MovieRepository extends JpaRepository<R_movie, Integer> {
 
 }
