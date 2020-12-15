01c8b5d0d205865a0727f4c178da1444027ba58b
DESKTOP-NKSGQVR\수영 <csy0797@gmail.com>
2020-12-15 15:25:39 +0900

    R_member에 pass2 변수 추가, join css 위치 수정

--- src/main/java/com/oraclejava/spring/model/R_member.java
+++ src/main/java/com/oraclejava/spring/model/R_member.java
@@ -18,6 +18,7 @@ public class R_member {
 	
 	private String id;
 	private String pass;
+	private String pass2;
 	private String email;
 	private Date reg_date;
 	
@@ -55,6 +56,14 @@ public class R_member {
 
 	public void setPass(String pass) {
 		this.pass = pass;
+	}
+
+	public String getPass2() {
+		return pass2;
+	}
+
+	public void setPass2(String pass2) {
+		this.pass2 = pass2;
 	}
 
 	public String getEmail() {
