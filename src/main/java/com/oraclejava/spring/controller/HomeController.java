8325f7c5698a7bfd177d4681f88c0abec6c24bb3
doosanpark <erid3232@naver.com>
2020-12-15 14:29:36 +0900

    홈 화면 및 headr 수정

--- src/main/java/com/oraclejava/spring/controller/HomeController.java
+++ src/main/java/com/oraclejava/spring/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.oraclejava.spring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,20 +15,27 @@ import com.oraclejava.spring.dao.movie.MovieRepository;
 @Controller
 public class HomeController {
 
-	
-	  @Autowired
-	   private MovieRepository movieRepository;
-	
-	   @RequestMapping(value={"/home","/"},
-		         method=RequestMethod.GET)
-		   public ModelAndView body_center() {
-		      ModelAndView rec = new ModelAndView();
-		      rec.setViewName("home/main");
-		      List<R_movie> lists = 
-		            movieRepository.findAll();
-		      rec.addObject("movieRecommend", lists);
-		      
-		      return rec;
-		   }
+	@Autowired
+	private MovieRepository movieRepository;
+
+	@RequestMapping(value = { "/home", "/" }, method = RequestMethod.GET)
+	public ModelAndView body_center() {
+		ModelAndView rec = new ModelAndView();
+		rec.setViewName("home/main");
+		List<R_movie> lists = movieRepository.findAll();
+		rec.addObject("movieRecommend", lists);
+
+		
+		List<R_movie> movieLists = movieRepository.findGenre("로맨스", PageRequest.of(0, 4));
+		List<R_movie> movieLists2  = movieRepository.findGenre("액션", PageRequest.of(0, 4));
+		List<R_movie> movieLists3  = movieRepository.findGenre("애니메이션", PageRequest.of(0, 4));
+		List<R_movie> movieLists4  = movieRepository.findGenre("코미디", PageRequest.of(0, 4));
+		rec.addObject("movieListRomance", lists);
+		rec.addObject("movieListAction", movieLists2);
+		rec.addObject("movieListAni", movieLists3);
+		rec.addObject("movieListComi", movieLists4);
+		
+		return rec;
+	}
 
 }
