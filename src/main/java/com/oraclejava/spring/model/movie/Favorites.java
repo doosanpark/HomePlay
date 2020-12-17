3a315cb3bb769c6aa804286b32b45a76c5acb4c8
ycchoi3213 <chk3213@naver.com>
2020-12-17 18:23:29 +0900

    Favorites 파일 추가

---
+++ src/main/java/com/oraclejava/spring/model/movie/Favorites.java
@@ -0,0 +1,29 @@
+package com.oraclejava.spring.model.movie;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity(name="v_fm")
+public class Favorites {
+	@Id
+	private int sn;
+	private String id;
+	private int rno;
+	private int no;
+    private String title;
+    private String genre;
+    private String summary_title;
+    private String summary_content;
+    private String age_limit;
+    private String poster;
+    private String thumbnail;
+    private String screening;
+    private String reg_date;
+	
+	
+}
