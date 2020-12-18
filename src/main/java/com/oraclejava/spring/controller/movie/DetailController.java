acf5bd2dd9b8434c4d78ab61cecb24f70f0fc531
DESKTOP-NKSGQVR\A <zlstjq@gmail.com>
2020-12-18 16:02:33 +0900

    상세화면 최종

--- src/main/java/com/oraclejava/spring/controller/movie/DetailController.java
+++ src/main/java/com/oraclejava/spring/controller/movie/DetailController.java
@@ -19,26 +19,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.oraclejava.spring.bean.movie.Movie;
 import com.oraclejava.spring.dao.movie.DetailRepository;
+import com.oraclejava.spring.dao.movie.MovieStaffRepository;
 import com.oraclejava.spring.dao.movie.Movie_favoriteRepository;
 import com.oraclejava.spring.model.movie.Detail;
 import com.oraclejava.spring.model.movie.RReview;
+import com.oraclejava.spring.model.movie.RStaff;
 import com.oraclejava.spring.model.movie.Favorite;
 
 @Controller
 public class DetailController {
 
 	@Autowired
-	private DetailRepository detailRepository;
+	private DetailRepository detailRepository; // 영화 및 댓글 정보
+
 	@Autowired
-	private Movie_favoriteRepository favoriteRepository;
+	private MovieStaffRepository movieStaffRepository; // 영화 출연진 정보
 
 	/*
 	 * @RequestMapping(value="/detail/detail", method=RequestMethod.GET) public
 	 * String indexTop() { return "redirect:/detail/detail/1"; }
 	 */
 
-	@RequestMapping(value = "/detail/{NO}", method = RequestMethod.GET)
+	@RequestMapping(value = "/detail/{NO}", method = RequestMethod.GET) // 초기 화면 출력
 	public ModelAndView index(@PathVariable Integer NO, HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView();
 
@@ -49,34 +53,38 @@ public class DetailController {
 		mav.addObject("detail", detailRepository.findById(NO).get());
 		mav.addObject("favoriteCount", detailRepository.count_favorite(s_id));
 
+		List<RStaff> list = movieStaffRepository.findStaff(NO);
+		mav.addObject("listStaff", list);
+
 		return mav;
 	}
 
-	@RequestMapping(path = "/detail/favorite", method = RequestMethod.POST)
+	@RequestMapping(path = "/detail/favorite", method = RequestMethod.POST) // 관심 등록 로직
 	public String favorite(@ModelAttribute Favorite favorite, @RequestParam Integer count, BindingResult bindingResult,
 			Model model, HttpServletRequest request) {
-		
 
-		HttpSession session = request.getSession();
-		String s_id = (String) session.getAttribute("user_id");
-        
-		favorite.setId(s_id);
+		try {
 
-		if (count == 0) {
+			HttpSession session = request.getSession();
+			String s_id = (String) session.getAttribute("user_id");
+			favorite.setId(s_id);
 
+			if (count == 0) {
+				Detail detail = detailRepository.findById(favorite.getDetail().getNo()).get();
+				detail.getFavorites().add(favorite);
+				detailRepository.save(detail);
+			}
 			
-			Detail detail = detailRepository.findById(favorite.getDetail().getNo()).get();
-			detail.getFavorites().add(favorite);
-			detailRepository.save(detail);
-		}
-		
-		
+			return "redirect:/detail/" + favorite.getDetail().getNo();
 
-		return "redirect:/detail/" + favorite.getDetail().getNo();
+		} catch (Exception e) {
+			return "redirect:/movie/detail/error";
+		}
+
 
 	}
 
-	@RequestMapping(path = "/detail/{NO}", method = RequestMethod.POST)
+	@RequestMapping(path = "/detail/{NO}", method = RequestMethod.POST) // 댓글 작성 로직
 	public String review(@ModelAttribute RReview r_review, BindingResult bindingResult, Model model,
 			HttpServletRequest request) {
 		int movie_no = r_review.getDetail().getNo();
@@ -86,16 +94,18 @@ public class DetailController {
 		Detail detail = detailRepository.findById(movie_no).get();
 
 		Date time = new Date();
+		try {
+			HttpSession session = request.getSession();
+			String s_id = (String) session.getAttribute("user_id");
+			int s_no = (int) session.getAttribute("user_no");
+			detail.getReview().add(new RReview(s_no, title, review, grade, time, detail, s_id));
+			detailRepository.save(detail);
+			return "redirect:/detail/" + movie_no;
 
-		HttpSession session = request.getSession();
-		String s_id = (String) session.getAttribute("user_id");
-		int s_no = (int) session.getAttribute("user_no");
+		} catch (Exception e) {
+			return "movie/detail/error";
+		}
 
-		detail.getReview().add(new RReview(s_no, title, review, grade, time, detail, s_id));
-
-		detailRepository.save(detail);
-
-		return "redirect:/detail/" + movie_no;
 	}
 
 }
--- src/main/java/com/oraclejava/spring/dao/movie/DetailRepository.java
+++ src/main/java/com/oraclejava/spring/dao/movie/DetailRepository.java
@@ -1,14 +1,19 @@
 package com.oraclejava.spring.dao.movie;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.oraclejava.spring.bean.movie.Movie;
 import com.oraclejava.spring.model.movie.Detail;
+import com.oraclejava.spring.model.movie.RStaff;
 
 public interface DetailRepository extends JpaRepository<Detail, Integer> {
 	
     @Query("select count(f.sn) from Favorite f where f.id =:id") 
 	public int count_favorite(String id);
+    
     
 }
