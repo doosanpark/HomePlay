eb20e84e4fcb7a0743a76b976115e1d22ec9fc06
ycchoi3213 <chk3213@naver.com>
2020-12-18 15:47:33 +0900

    MyPageController 코드 주석 처리(미사용)

--- src/main/java/com/oraclejava/spring/controller/MyPageController.java
+++ src/main/java/com/oraclejava/spring/controller/MyPageController.java
@@ -7,36 +7,51 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.oraclejava.spring.bean.movie.Movie;
+import com.oraclejava.spring.dao.movie.FavoritesRepository;
+import com.oraclejava.spring.dao.movie.MovieRepository;
 import com.oraclejava.spring.model.R_member;
+import com.oraclejava.spring.model.movie.Favorites;
+
 
 @Controller
 public class MyPageController {
 	
 	@Autowired
 	private UserRepository userRepository;
-	
-	@RequestMapping(value = { "account/mypage" }, method = RequestMethod.GET)
-	public ModelAndView mypage(@ModelAttribute R_member form, HttpServletRequest request, HttpServletResponse response) {
-	
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("account/mypage");	
-		
-	HttpSession session = request.getSession();
-	String id = (String)session.getAttribute("user_id");
+	@Autowired
+	private FavoritesRepository favoritesRepository;
 
-	System.out.println(id);
 	
-	
-	List<R_member> user_info = userRepository.findUser(id);
-	mav.addObject("user_info", user_info);
-	
-	return mav;
-	}
+//	@RequestMapping(value = { "account/mypage" }, method = RequestMethod.GET)
+//	public ModelAndView mypage(@ModelAttribute R_member form, HttpServletRequest request, HttpServletResponse response) {
+//	
+//
+//		ModelAndView mav = new ModelAndView();
+//		mav.setViewName("account/mypage");	
+//		
+//	HttpSession session = request.getSession();
+//	String id = (String)session.getAttribute("user_id");
+//
+//	List<R_member> user_info = userRepository.findUser(id);
+//	mav.addObject("user_info", user_info);
+//	
+//	List<Favorites> findFavorite = favoritesRepository.findFavorite(id);
+//	mav.addObject("findFavorite", findFavorite);
+//
+//
+//	return mav;
+//	}
 
+	
 }
