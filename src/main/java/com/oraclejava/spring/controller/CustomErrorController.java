e4beeded46bc64a711ab9a54d5f7102bcc5d65c7
ycchoi3213 <chk3213@naver.com>
2020-12-18 15:04:17 +0900

    에러페이지 customize (404, 403, 500, 나머지 기타 에러페이지)

---
+++ src/main/java/com/oraclejava/spring/controller/CustomErrorController.java
@@ -0,0 +1,40 @@
+package com.oraclejava.spring.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+	@RequestMapping("/error")
+	public ModelAndView handleError(HttpServletRequest request, HttpServletResponse response) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		ModelAndView modelAndView = new ModelAndView();
+		System.out.println(response.getStatus());
+		if (status != null) {
+			Integer statusCode = Integer.valueOf(status.toString());
+			if (statusCode == HttpStatus.NOT_FOUND.value()) {
+				modelAndView.setViewName("/error/404");
+			} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				modelAndView.setViewName("/error/500");
+			} else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+				modelAndView.setViewName("/error/403");
+			}else 
+				modelAndView.setViewName("/error/common");
+			
+		}
+		return modelAndView;
+	}
+	
+	@Override
+	public String getErrorPath() {
+		return null;
+	}
+}
