4d87a78750f16e5e6d1bc10ffd287d0ffecb97a5
YANG95 <adolf9608@gmail.com>
2020-12-18 15:14:59 +0900

    무비리스트수정, 썸네일?

--- src/main/java/com/oraclejava/spring/controller/movie/MovieController.java
+++ src/main/java/com/oraclejava/spring/controller/movie/MovieController.java
@@ -31,17 +31,17 @@ public class MovieController {
          ModelAndView mav = new ModelAndView();
          mav.setViewName("movie/list/movieList");
          List<Movie> lists = 
-               movieRepository.findGenre("로맨스", PageRequest.of(0, 4));
+               movieRepository.findGenre("로맨스", PageRequest.of(0, 6));
          List<Movie> lists2 = 
-                  movieRepository.findGenre("액션", PageRequest.of(0, 4));
+                  movieRepository.findGenre("액션", PageRequest.of(0, 6));
          List<Movie> lists3 = 
-               movieRepository.findGenre("애니메이션", PageRequest.of(0, 4));
+               movieRepository.findGenre("애니메이션", PageRequest.of(0, 6));
          List<Movie> lists4 = 
-               movieRepository.findGenre("코미디", PageRequest.of(0, 4));
+               movieRepository.findGenre("SF", PageRequest.of(0, 6));
          mav.addObject("movieListRomance", lists);
          mav.addObject("movieListAction", lists2);
          mav.addObject("movieListAni", lists3);
-         mav.addObject("movieListComi", lists4);
+         mav.addObject("movieListSf", lists4);
          
          return mav;
       }
