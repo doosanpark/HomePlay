639134838599aa8fb26945e1098e6423f5fc99b7
ycchoi3213 <chk3213@naver.com>
2020-12-22 14:34:53 +0900

    홈화면 드라마 출력 구현

--- src/main/java/com/oraclejava/spring/controller/HomeController.java
+++ src/main/java/com/oraclejava/spring/controller/HomeController.java
@@ -3,6 +3,7 @@
 
 package com.oraclejava.spring.controller;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.oraclejava.spring.dao.drama.DramaRepository;
 import com.oraclejava.spring.dao.movie.MovieRepository;
+import com.oraclejava.spring.model.drama.Drama;
 import com.oraclejava.spring.model.movie.Movie;
 
 @Controller
@@ -22,32 +25,63 @@ public class HomeController {
 	@Autowired
 	private MovieRepository movieRepository;
 
+	@Autowired
+	private DramaRepository dramaRepository;
+
 	@RequestMapping(value = { "/home", "/" }, method = RequestMethod.GET)
 	public ModelAndView body_center() {
 		ModelAndView rec = new ModelAndView();
 		rec.setViewName("home/main");
-		List<Movie> lists = movieRepository.findAll();
-		rec.addObject("movieRecommend", lists);
 
-		Collections.shuffle(lists); // 리스트 내 값 랜덤으로 순서 재배치
-
-		// 총 영화 리스트 중 4개 항목만 리스트에 저장
-		int listLen = lists.size();
-		for (int i = listLen-1; i >= 4; i--) {
-			lists.remove(i);
+		List M_lists = movieRepository.findAll(); // 영화 리스트
+		List D_lists = dramaRepository.findAll(); // 드라마 리스트
+//		Collections.shuffle(M_lists); // 영화 리스트 내 값 랜덤으로 순서 재배치
+//		Collections.shuffle(D_lists); // 드라마 리스트 내 값 랜덤으로 순서 재배치
+		
+		
+		
+		
+		
+		// 총 영화 리스트 중 2개 항목만 리스트에 저장
+		int M_listLen = M_lists.size();
+		for (int i = 1; i <= M_listLen - 2; i++) {
+			M_lists.remove(2);
 
 		}
 
-		rec.addObject("movieRecommend", lists);
+		// 총 드라마 리스트 중 1개 항목만 리스트에 저장
+		int D_listLen = D_lists.size();
+		for (int i = 1; i <= D_listLen - 1; i++) {
+			D_lists.remove(1);
 
-		List<Movie> movieLists = movieRepository.findGenre("로맨스", PageRequest.of(0, 4));
+		}
+
+		rec.addObject("movieRecommend", M_lists);
+		rec.addObject("dramaRecommend", D_lists);
+//
+//		List totalLists = new ArrayList();
+//		totalLists.add(M_lists);
+//		totalLists.add(D_lists);
+//		Collections.shuffle(totalLists); // 영화 리스트 내 값 랜덤으로 순서 재배치
+//		// 총 영화, 드라마 리스트 중 4개 항목만 리스트에 저장
+//		int totalListLen = totalLists.size();
+//		for (int i = 1; i <= 2; i++) {
+//			for (int j = 1; j <= totalListLen - 4; j++) {
+//				totalLists.remove(4);
+//			}
+//		}
+//		rec.addObject("Recommend", totalLists);
+//		System.out.println(totalLists);
+
+		List<Movie> movieLists1 = movieRepository.findGenre("로맨스", PageRequest.of(0, 4));
 		List<Movie> movieLists2 = movieRepository.findGenre("액션", PageRequest.of(0, 4));
-		List<Movie> movieLists3 = movieRepository.findGenre("애니메이션", PageRequest.of(0, 4));
-		List<Movie> movieLists4 = movieRepository.findGenre("SF", PageRequest.of(0, 4));
-		rec.addObject("movieListRomance", movieLists);
+		List<Drama> dramaLists1 = dramaRepository.findGenre("스릴러", PageRequest.of(0, 4));
+		List<Drama> dramaLists2 = dramaRepository.findGenre("코미디", PageRequest.of(0, 4));
+	
+		rec.addObject("movieListRomance", movieLists1);
 		rec.addObject("movieListAction", movieLists2);
-		rec.addObject("movieListAni", movieLists3);
-		rec.addObject("movieListSf", movieLists4);
+		rec.addObject("dramaThriller", dramaLists1);
+		rec.addObject("dramaComedy", dramaLists2);
 
 		return rec;
 	}
