7de86647f00abcae1eb4eed362d7f038ff87d455
ycchoi3213 <chk3213@naver.com>
2020-12-22 17:00:28 +0900

    게임 항목 화면 출력 수정

--- src/main/java/com/oraclejava/spring/controller/HomeController.java
+++ src/main/java/com/oraclejava/spring/controller/HomeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.oraclejava.spring.dao.drama.DramaRepository;
+import com.oraclejava.spring.dao.game.GameRepository;
 import com.oraclejava.spring.dao.movie.MovieRepository;
 import com.oraclejava.spring.model.drama.Drama;
 import com.oraclejava.spring.model.movie.Movie;
@@ -28,6 +29,9 @@ public class HomeController {
 	@Autowired
 	private DramaRepository dramaRepository;
 
+	@Autowired
+	private GameRepository gameRepository;
+
 	@RequestMapping(value = { "/home", "/" }, method = RequestMethod.GET)
 	public ModelAndView body_center() {
 		ModelAndView rec = new ModelAndView();
@@ -35,13 +39,10 @@ public class HomeController {
 
 		List M_lists = movieRepository.findAll(); // 영화 리스트
 		List D_lists = dramaRepository.findAll(); // 드라마 리스트
+		List G_lists = gameRepository.findAll(); // 게임 리스트
 //		Collections.shuffle(M_lists); // 영화 리스트 내 값 랜덤으로 순서 재배치
 //		Collections.shuffle(D_lists); // 드라마 리스트 내 값 랜덤으로 순서 재배치
-		
-		
-		
-		
-		
+
 		// 총 영화 리스트 중 2개 항목만 리스트에 저장
 		int M_listLen = M_lists.size();
 		for (int i = 1; i <= M_listLen - 2; i++) {
@@ -56,8 +57,16 @@ public class HomeController {
 
 		}
 
+		// 총 게임 리스트 중 1개 항목만 리스트에 저장
+		int G_listLen = G_lists.size();
+		for (int i = 1; i <= G_listLen - 1; i++) {
+			G_lists.remove(1);
+
+		}
+
 		rec.addObject("movieRecommend", M_lists);
 		rec.addObject("dramaRecommend", D_lists);
+		rec.addObject("gameRecommend", G_lists);
 //
 //		List totalLists = new ArrayList();
 //		totalLists.add(M_lists);
@@ -77,7 +86,7 @@ public class HomeController {
 		List<Movie> movieLists2 = movieRepository.findGenre("액션", PageRequest.of(0, 4));
 		List<Drama> dramaLists1 = dramaRepository.findGenre("스릴러", PageRequest.of(0, 4));
 		List<Drama> dramaLists2 = dramaRepository.findGenre("코미디", PageRequest.of(0, 4));
-	
+
 		rec.addObject("movieListRomance", movieLists1);
 		rec.addObject("movieListAction", movieLists2);
 		rec.addObject("dramaThriller", dramaLists1);
